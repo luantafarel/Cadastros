c7edf2f2aeb49e47428d33c08b6f4ab2199b57a6
Luan Tafarel Da Silva <luantafarel7@outlook.com>
2020-01-27 12:13:56 -0300

    Adjust to work in heroku

--- src/config/db.js
+++ src/config/db.js
@@ -6,13 +6,14 @@ const user = 'admin'
 const password = 'test'
 
 const connection = {
+  mongoHeroku: 'mongodb://heroku_45wr8jhl:88960074tel@ds143767.mlab.com:43767/heroku_45wr8jhl',
   mongoAtlas: `mongodb+srv://${user}:${password}@cluster0-e7yyt.mongodb.net/${collection}?retryWrites=true&w=majority`,
   mongoLocal: `mongodb://localhost:27017/${collection}`
 }
 exports.connect = function() {
   return new Promise((resolve, reject) => {
     mongoose
-      .connect(connection.mongoLocal, {
+      .connect(connection.mongoHeroku, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
