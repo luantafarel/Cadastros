4e19a5f60a72edd79e0c352adc46a6bc159fe86e
Luan Tafarel Da Silva <luantafarel7@outlook.com>
2020-01-27 15:34:45 -0300

    Adjust to work in heroku

--- src/config/db.js
+++ src/config/db.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose')
 const logger = require('./winston').logger
 const collection = 'test'
 const user = 'admin'
-const password = '2GFCikqDsTe9eoHA'
+const password = 'admin'
 
 const connection = {
-  mongoAtlas: `mongodb+srv://${user}:${password}@cluster0-cygcu.mongodb.net/${collection}?w=majority`,
+  mongoAtlas: `mongodb+srv://${user}:${password}@cluster0-cygcu.mongodb.net/test?retryWrites=true&w=majority`,
   mongoLocal: `mongodb://localhost:27017/${collection}`
 }
 exports.connect = function() {
   return new Promise((resolve,reject) => {
     mongoose
-      .createConnection(connection.mongoAtlas,{
+      .connect(connection.mongoAtlas,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true
+        connectTimeoutMS: 1000
       })
       .then(function() {
         logger.info('MongoDB connected!')
